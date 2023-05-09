74983f96fee9230e22c0a4f0414478e05de16bb6
gabriel fernandes <gabriel727fernandes@gmail.com>
2023-05-09 18:37:17 -0300

    Project created

---
+++ src/main/java/com/gabriel/mongodb/WorskshopmongoApplication.java
@@ -0,0 +1,13 @@
+package com.gabriel.mongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorskshopmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorskshopmongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gabriel/mongodb/WorskshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabriel.mongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorskshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
