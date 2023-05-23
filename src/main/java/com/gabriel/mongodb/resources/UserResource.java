614c8de5cb81c36fe2eff741ac4f2f56c37da2ab
gabriel fernandes <gabriel727fernandes@gmail.com>
2023-05-23 17:23:19 -0300

    insert user with post

--- src/main/java/com/gabriel/mongodb/resources/UserResource.java
+++ src/main/java/com/gabriel/mongodb/resources/UserResource.java
@@ -1,14 +1,17 @@
 package com.gabriel.mongodb.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.gabriel.mongodb.domain.User;
 import com.gabriel.mongodb.dto.UserDTO;
@@ -31,11 +34,18 @@ public class UserResource {
 	}
 	
 	@RequestMapping(value="/{id}", method=RequestMethod.GET)
-	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+ 	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
 		User obj = service.findById(id);
-	
 		return ResponseEntity.ok().body(new UserDTO(obj));
-		
 	}
 
+	@RequestMapping(method=RequestMethod.POST)
+ 	public ResponseEntity<Void> insert(@RequestBody UserDTO objDto) {
+		User obj = service.fromDTO(objDto);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+
+	
 }
