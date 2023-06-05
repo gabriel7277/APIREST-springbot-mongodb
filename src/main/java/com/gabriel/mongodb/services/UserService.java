83e662b3b0bea66566374c6bf0e09fbcd73c1fe9
gabriel fernandes <gabriel727fernandes@gmail.com>
2023-06-05 08:13:26 -0300

    user update with PUT

--- src/main/java/com/gabriel/mongodb/services/UserService.java
+++ src/main/java/com/gabriel/mongodb/services/UserService.java
@@ -35,6 +35,20 @@ public class UserService {
 		findById(id);
 		repo.deleteById(id);
 	}
+	
+	public User update(User entity) {
+
+	     User newObj = repo.findById(entity.getId()).get();
+
+	     updateData(newObj, entity);
+
+	     return repo.save(newObj);
+
+	}
+	private void updateData(User newObj, User obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
+	}
 
 	public User fromDTO(UserDTO objDto) {
 		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
