534f0fdd974055e2e02eedbf37a58e5d22de8c41
gabriel fernandes <gabriel727fernandes@gmail.com>
2023-08-16 14:55:24 -0300

    Getting a post by id

---
+++ src/main/java/com/gabriel/mongodb/resources/PostResource.java
@@ -0,0 +1,30 @@
+package com.gabriel.mongodb.resources;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.gabriel.mongodb.domain.Post;
+import com.gabriel.mongodb.dto.UserDTO;
+import com.gabriel.mongodb.services.PostService;
+
+
+@RestController
+@RequestMapping(value="/posts")
+public class PostResource {
+	
+	@Autowired
+	private PostService service;
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+ 	public ResponseEntity<Optional<Post>> findById(@PathVariable String id) {
+		Optional<Post> obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+
+}
---
+++ src/main/java/com/gabriel/mongodb/services/PostService.java
@@ -0,0 +1,32 @@
+package com.gabriel.mongodb.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gabriel.mongodb.domain.Post;
+import com.gabriel.mongodb.exception.ObjectNotFoundException;
+import com.gabriel.mongodb.repository.PostRepository;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+	
+
+	public Optional<Post> findById(String id) {
+
+		Optional<Post> user = repo.findById(id);
+
+		if(user == null) {
+
+		throw new ObjectNotFoundException("Objeto não encontrado");
+
+		}
+
+		return user;
+	   }
+	}
+	
