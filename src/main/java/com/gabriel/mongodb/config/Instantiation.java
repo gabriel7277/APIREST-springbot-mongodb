c4f8641e7f6402fc755db1c0f05ea024a2fe625f
gabriel fernandes <gabriel727fernandes@gmail.com>
2023-08-16 15:13:04 -0300

    Adding comments to posts

--- src/main/java/com/gabriel/mongodb/config/Instantiation.java
+++ src/main/java/com/gabriel/mongodb/config/Instantiation.java
@@ -12,6 +12,7 @@ import org.springframework.context.annotation.Configuration;
 import com.gabriel.mongodb.domain.Post;
 import com.gabriel.mongodb.domain.User;
 import com.gabriel.mongodb.dto.AuthorDTO;
+import com.gabriel.mongodb.dto.CommentDTO;
 import com.gabriel.mongodb.repository.PostRepository;
 import com.gabriel.mongodb.repository.UserRepository;
 
@@ -36,12 +37,18 @@ import com.gabriel.mongodb.repository.UserRepository;
 
 			User gabriel = new User(null, "Gabriel Fernandes", "gabriel@gmail.com");
 			User Gabriel  = new User(null, "Gabriel ", "gabriel@gmail.com");
+			User exemplo  = new User(null, "exemplo ", "exemplo@gmail.com");
 			
 			userRepository.saveAll(Arrays.asList(gabriel, Gabriel));
 
 			Post post10 = new Post(null, sdf.parse("22/03/2018"), "Partiu viagem", "Vou viajar para  o Rio de Janeiro.",new AuthorDTO(gabriel));
-
 			Post post20 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!",  new AuthorDTO(gabriel));
+			
+			CommentDTO c10 = new CommentDTO("Boa viagem mano!", sdf.parse("22/03/2022"), new AuthorDTO(Gabriel));
+			CommentDTO c20 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2022"), new AuthorDTO(exemplo));
+			
+			post10.getComments().addAll(Arrays.asList(c10));
+			post20.getComments().addAll(Arrays.asList(c20));
 
 			postRepository.saveAll(Arrays.asList(post10, post20));
 			
--- src/main/java/com/gabriel/mongodb/domain/Post.java
+++ src/main/java/com/gabriel/mongodb/domain/Post.java
@@ -1,12 +1,15 @@
 package com.gabriel.mongodb.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.gabriel.mongodb.dto.AuthorDTO;
+import com.gabriel.mongodb.dto.CommentDTO;
 
 	
 
@@ -21,6 +24,8 @@ private static final long serialVersionUID = 1L;
 		private String title;
 		private String body;
 		private AuthorDTO author;
+		
+		private List<CommentDTO> comments = new ArrayList<>();	
 		
 		public Post() {
 			
@@ -73,6 +78,14 @@ private static final long serialVersionUID = 1L;
 
 		public void setAuthor(AuthorDTO author) {
 			this.author = author;
+		}
+		
+		public List<CommentDTO> getComments() {
+			return comments;
+		}
+
+		public void setComments(List<CommentDTO> comments) {
+			this.comments = comments;
 		}
 
 		@Override
