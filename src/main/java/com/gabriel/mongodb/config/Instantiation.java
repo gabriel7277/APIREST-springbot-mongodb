70882119de0ab10d17b78fcf4995eac02ccde389
gabriel fernandes <gabriel727fernandes@gmail.com>
2023-08-16 13:52:29 -0300

    Author data projection with DTO

--- src/main/java/com/gabriel/mongodb/config/Instantiation.java
+++ src/main/java/com/gabriel/mongodb/config/Instantiation.java
@@ -12,6 +12,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.gabriel.mongodb.domain.Post;
 import com.gabriel.mongodb.domain.User;
+import com.gabriel.mongodb.dto.AuthorDTO;
 import com.gabriel.mongodb.repository.PostRepository;
 import com.gabriel.mongodb.repository.UserRepository;
 
@@ -32,16 +33,17 @@ import com.gabriel.mongodb.repository.UserRepository;
 			
 			userRepository.deleteAll();
 			postRepository.deleteAll();
+			
 
 			User gabriel = new User(null, "Gabriel Ribeiro", "gabriel@gmail.com");
 			User Gabriel  = new User(null, "Gabriel Fernandes", "gabriel@gmail.com");
 			
-			
-			Post post10 = new Post(null, sdf.parse("22/03/2018"), "Partiu viagem", "Vou viajar para  o Rio de Janeiro.",  gabriel);
-
-			Post post20 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!",  gabriel);
-
 			userRepository.saveAll(Arrays.asList(gabriel, Gabriel));
+
+			Post post10 = new Post(null, sdf.parse("22/03/2018"), "Partiu viagem", "Vou viajar para  o Rio de Janeiro.",new AuthorDTO(gabriel));
+
+			Post post20 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!",  new AuthorDTO(Gabriel));
+
 			postRepository.saveAll(Arrays.asList(post10, post20));
 		}
 
